024da1c40983ee4ec0396d85301a439c139d2b4d
huwence <huwence@gmail.com>
2014-03-16 21:54:46 +0800

    1. Add FPS parameter. 2. Adjust rotate angle value

--- simpleCube.js
+++ simpleCube.js
@@ -6,7 +6,7 @@
  *
  * */
 
-(function (){
+(function () {
 
         //the length of cube
     var L = 200,   
@@ -32,139 +32,152 @@
         //the width of canvas
         WIDTH = 400,
         //context of canvas
-        CONTEXT;
+        CONTEXT,
+        //FPS
+        FPS = 60,
+        //INTERVAL
+        INTERVAL = 1000 / FPS
 
-    var RegPath = /^([A-H])([A-H])([A-H])([A-H])$/;
+    var RegPath = /^([A-H])([A-H])([A-H])([A-H])$/
 
     //request animation frame
-    var _requestFrame = (function (){
+    var _requestFrame = (function () {
         return  window.requestAnimationFrame       ||   
                 window.webkitRequestAnimationFrame ||   
                 window.mozRequestAnimationFrame    ||   
                 window.oRequestAnimationFrame      ||   
                 window.msRequestAnimationFrame     ||   
                 function (callback){  
-                    window.setTimeout(callback, 1000/60);  
-                };
-    })();
+                    window.setTimeout(callback, 1000/60)  
+                }
+    })()
 
-    function drawSurface (path, color){
-        var result = RegPath.exec(path); 
+    function drawSurface (path, color) {
+        var result = RegPath.exec(path) 
 
         if (!result)
-            return;
+            return
 
         var point1 = getProjectionPoint(result[1]),
             point2 = getProjectionPoint(result[2]),
             point3 = getProjectionPoint(result[3]),
-            point4 = getProjectionPoint(result[4]);
+            point4 = getProjectionPoint(result[4])
 
-        CONTEXT.fillStyle = color || '#000';
-        CONTEXT.beginPath();
-        CONTEXT.moveTo(point1[0], point1[1]);
-        CONTEXT.lineTo(point2[0], point2[1]);
-        CONTEXT.lineTo(point3[0], point3[1]);
-        CONTEXT.lineTo(point4[0], point4[1]);
-        CONTEXT.closePath();
-        CONTEXT.stroke();
-        CONTEXT.fill();
+        CONTEXT.fillStyle = color || '#000'
+		CONTEXT.strokeStyle = '#000';
+        CONTEXT.beginPath()
+        CONTEXT.moveTo(point1[0], point1[1])
+        CONTEXT.lineTo(point2[0], point2[1])
+        CONTEXT.lineTo(point3[0], point3[1])
+        CONTEXT.lineTo(point4[0], point4[1])
+        CONTEXT.lineTo(point1[0], point1[1])
+        CONTEXT.fill()
+        CONTEXT.stroke()
     }
 
     //return the projetion point in canvas
-    function getProjectionPoint (point){
+    function getProjectionPoint (point) {
         if (!POINTS[point])
-            return false;
+            return false
 
         var x = POINTS[point][0],
             y = POINTS[point][1],
-            z = POINTS[point][2];
+            z = POINTS[point][2]
 
         //projection point
         var pX = x * PERSPECTIVE / (Math.abs(CZ - z)) + WIDTH / 2,
-            pY = y * PERSPECTIVE / (Math.abs(CZ - z)) + HEIGHT / 2;
+            pY = y * PERSPECTIVE / (Math.abs(CZ - z)) + HEIGHT / 2
 
-        return [pX, pY];
+        return [pX, pY]
     }
 
-    function rotate (point, angles){
+    function rotate (point, angles) {
         if (!POINTS[point])
-            return false;
+            return false
 
         if (!angles)
-            angles = {x: 0, y: 0, z: 0};
+            angles = {x: 0, y: 0, z: 0}
 
         var x = POINTS[point][0],
             y = POINTS[point][1],
-            z = POINTS[point][2];
+            z = POINTS[point][2]
 
         //rotate X axis
-        if (angles.x){
-            var temp_y = y;
+        if (angles.x) {
+            var temp_y = y
 
-            angles.x *= Math.PI / 180;
-            y = y * Math.cos(angles.x) - z * Math.sin(angles.x);
-            z = temp_y * Math.sin(angles.x) + z * Math.cos(angles.x);
+            angles.x *= Math.PI / 180
+            y = y * Math.cos(angles.x) + z * Math.sin(angles.x)
+            z = z * Math.cos(angles.x) - temp_y * Math.sin(angles.x)
         }
 
         //rotate Y axis
-        if (angles.y){
-            var temp_x = x;
+        if (angles.y) {
+            var temp_x = x
 
-            angles.y *= Math.PI / 180;
-            x = x * Math.cos(angles.y) - z * Math.sin(angles.y);
-            z = temp_x * Math.sin(angles.y) + z * Math.cos(angles.y);
+            angles.y *= Math.PI / 180
+            x = x * Math.cos(angles.y) - z * Math.sin(angles.y)
+            z = temp_x * Math.sin(angles.y) + z * Math.cos(angles.y)
         }
 
         //rotate Z axis
-        if (angles.z){
-            var temp_x = x;
+        if (angles.z) {
+            var temp_x = x
 
-            angles.z *= Math.PI / 180;
-            x = x * Math.cos(angles.z) - y * Math.sin(angles.z);
-            y = temp_x * Math.sin(angles.z) + y * Math.cos(angles.z);
+            angles.z *= Math.PI / 180
+            x = x * Math.cos(angles.z) - y * Math.sin(angles.z)
+            y = temp_x * Math.sin(angles.z) + y * Math.cos(angles.z)
         }
 
-        POINTS[point][0] = x;
-        POINTS[point][1] = y; 
-        POINTS[point][2] = z;
+        POINTS[point][0] = x
+        POINTS[point][1] = y 
+        POINTS[point][2] = z
     }
 
     //we need to draw every surface on cube
-    function draw (){
-        CONTEXT.clearRect(0, 0, WIDTH, HEIGHT);
+    function draw () {
+        CONTEXT.clearRect(0, 0, WIDTH, HEIGHT)
         //draw AEFB
-        drawSurface('AEFB', '#33ff33');
+        drawSurface('AEFB', '#33ff33')
         //draw DHGC
-        drawSurface('DHGC', '#ffff2a');
+        drawSurface('DHGC', '#ffff2a')
         //draw CGFB
-        drawSurface('CGFB', '#99cc99');
+        drawSurface('CGFB', '#99cc99')
         //draw DHEA
-        drawSurface('DHEA', '#66cc66');
+        drawSurface('DHEA', '#66cc66')
         //draw HEFG
-        drawSurface('HEFG', '#2a2aff');
+        drawSurface('HEFG', '#2a2aff')
         //draw ABCD
-        drawSurface('ABCD', '#ff2aaa');
+        drawSurface('ABCD', '#ff2aaa')
     }
 
     function initialize(){
-       var canvas = document.createElement('canvas');   
-       canvas.height = HEIGHT;
-       canvas.width= WIDTH;
-       CONTEXT = canvas.getContext('2d');
-       document.body.appendChild(canvas);
+       var canvas = document.createElement('canvas'),
+           start = +new Date()
+
+       canvas.height = HEIGHT
+       canvas.width = WIDTH
+       CONTEXT = canvas.getContext('2d')
+       document.body.appendChild(canvas)
 
        function autoRotate (){
-           for (var p in POINTS)
-               rotate(p, {x: 1, y: 1, z: 1});
+           var now = +new Date(),
+               delta = now - start
 
-           draw();
-           _requestFrame(autoRotate);
-           //setTimeout(autoRotate, 300);
+           if (delta >= INTERVAL) {
+               for (var p in POINTS) {
+                   rotate(p, {x: 1.4, y: 2, z: 1.2})
+               }
+
+               draw()
+           }
+
+           _requestFrame(autoRotate)
        }
 
-       autoRotate();
+       autoRotate()
     }
 
-    initialize();
+    initialize()
 
-})();
+})()
